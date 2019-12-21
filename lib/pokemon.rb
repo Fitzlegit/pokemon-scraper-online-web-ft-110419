ccdc6e5a3bf1c4b315b7640353f43a82c0572d12
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-21 19:14:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+  end
+
+  def self.save
+    @@all << self
+  end
+
+  def self.find
+  end
 end
